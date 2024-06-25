9349cbeceafa4174ee0cb07376aa1482041d9e38
Daniel Pedersoli <dpmsengineer@gmail.com>
2024-06-24 22:46:56 -0300

    to be null

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,6 +3,6 @@ import { getStorageCity } from "@libs/asyncStorage/cityStorage"
 describe("Storage: CityStorage", () => {
   it("should be return null when don't have a city storage", async () => {
     const response = await getStorageCity()
-    console.log("response: ", response)
+    expect(response).toBeNull()
   })
 })
