a80f75daec025066fc98deb9ceb8f48775e51802
Daniel Pedersoli <dpmsengineer@gmail.com>
2024-06-27 21:29:40 -0300

    more to go

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,23 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { CityProps } from "@services/getCityByNameService"
 
 describe("Storage: CityStorage", () => {
   it("should be return null when don't have a city storage", async () => {
     const response = await getStorageCity()
     expect(response).toBeNull()
   })
-})
+
+  it("should be return city storage", async () => {
+    const newCity: CityProps = {
+      id: "1",
+      name: "São Paulo",
+      longitude: 123,
+      latitude: 456
+    }
+
+    await saveStorageCity(newCity)
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity)
+  });
+});
